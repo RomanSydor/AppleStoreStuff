4163bd5c710d81d351adda5b8ef1b56ec0fbcfa7
Roman <first.luker@gmail.com>
2020-08-24 15:05:16 +0300

    Fix in controller.

--- AppleStoreStuff/Controllers/MacController.cs
+++ AppleStoreStuff/Controllers/MacController.cs
@@ -32,5 +32,19 @@ namespace AppleStoreStuff.Controllers
             return View(mac);
         }
 
+        [HttpGet]
+        [Route("/Mac/Index/{model}/{memory}/{color}")]
+        public async Task<IActionResult> Index([FromRoute]string model, [FromRoute]string memory, [FromRoute]string color)
+        {
+            var mac = await _macRepository.GetMacsByModel(model, color, memory);
+
+            if (mac == null)
+            {
+                NotFound();
+            }
+
+            return View(mac);
+        }
+
     }
 }
